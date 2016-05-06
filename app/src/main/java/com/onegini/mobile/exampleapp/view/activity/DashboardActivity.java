7291ee46a261b59ade0997dde40ecdd8fd7e823e
Adam Chodera <adam@onegini.com>
2016-05-06 15:37:32 +0200

    EXAMPLEAND-6 add dashboard screen

--- app/src/main/java/com/onegini/mobile/exampleapp/view/activity/DashboardActivity.java
+++ app/src/main/java/com/onegini/mobile/exampleapp/view/activity/DashboardActivity.java
@@ -1,22 +1,47 @@
 package com.onegini.mobile.exampleapp.view.activity;
 
-import android.content.Context;
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import com.onegini.mobile.exampleapp.R;
 
 public class DashboardActivity extends AppCompatActivity {
+
+  public static void startActivity(@NonNull final Activity context) {
+    final Intent contactDetailsIntent = new Intent(context, DashboardActivity.class);
+    context.startActivity(contactDetailsIntent);
+    context.finish();
+  }
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_dashboard);
+    ButterKnife.bind(this);
+
   }
 
-  public static void startActivity(@NonNull final Context context) {
-    final Intent contactDetailsIntent = new Intent(context, DashboardActivity.class);
-    context.startActivity(contactDetailsIntent);
+  @OnClick(R.id.button_logout)
+  public void logout() {
+    showToast("logout");
+  }
+
+  @OnClick(R.id.button_disconnect)
+  public void disconnect() {
+    showToast("disconnect");
+  }
+
+  @OnClick(R.id.button_get_user_profile)
+  public void getUserProfileData() {
+    showToast("getUserProfileData");
+  }
+
+  private void showToast(final String message) {
+    Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
   }
 }
