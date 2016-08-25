2fee967642ad7adf5e14c9e36c528aa424cca43b
Robert Szuba <robert@onegini.com>
2016-08-25 09:46:49 +0200

    SDKAND-191 Use new handleMobileAuthenticationRequest method

--- app/src/main/java/com/onegini/mobile/exampleapp/network/gcm/GCMIntentService.java
+++ app/src/main/java/com/onegini/mobile/exampleapp/network/gcm/GCMIntentService.java
@@ -4,6 +4,9 @@ import android.app.IntentService;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
+import com.onegini.mobile.android.sdk.handlers.OneginiMobileAuthenticationHandler;
+import com.onegini.mobile.android.sdk.handlers.error.OneginiMobileAuthenticationError;
 import com.onegini.mobile.exampleapp.OneginiSDK;
 
 public class GCMIntentService extends IntentService {
@@ -19,7 +22,17 @@ public class GCMIntentService extends IntentService {
     final Bundle extras = intent.getExtras();
     if (!extras.isEmpty()) {
       Log.i(TAG, "Push message received");
-      OneginiSDK.getOneginiClient(this).getUserClient().handlePushNotification(extras);
+      OneginiSDK.getOneginiClient(this).getUserClient().handleMobileAuthenticationRequest(extras, new OneginiMobileAuthenticationHandler() {
+        @Override
+        public void onSuccess() {
+          Toast.makeText(GCMIntentService.this, "Mobile authentication success", Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onError(final OneginiMobileAuthenticationError oneginiMobileAuthenticationError) {
+          Toast.makeText(GCMIntentService.this, oneginiMobileAuthenticationError.getErrorDescription(), Toast.LENGTH_SHORT).show();
+        }
+      });
     }
     // Release the wake lock provided by the WakefulBroadcastReceiver.
     GCMBroadcastReceiver.completeWakefulIntent(intent);
