e75e48816257638cf5832eed807c75e5ba54c0aa
Robert Szuba <robert@onegini.com>
2016-09-26 11:30:03 +0200

    EXAMPLEAND-8 Add the license to new files

--- app/src/main/java/com/onegini/mobile/exampleapp/view/handler/MobileAuthenticationFingerprintRequestHandler.java
+++ app/src/main/java/com/onegini/mobile/exampleapp/view/handler/MobileAuthenticationFingerprintRequestHandler.java
@@ -1,18 +1,32 @@
+/*
+ * Copyright (c) 2016 Onegini B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.onegini.mobile.exampleapp.view.handler;
-
-import android.content.Context;
-import android.content.Intent;
-
-import com.onegini.mobile.exampleapp.view.activity.FingerprintActivity;
-import com.onegini.mobile.exampleapp.view.activity.MobileAuthenticationFingerprintActivity;
-import com.onegini.mobile.sdk.android.handlers.request.OneginiMobileAuthenticationFingerprintRequestHandler;
-import com.onegini.mobile.sdk.android.handlers.request.callback.OneginiFingerprintCallback;
-import com.onegini.mobile.sdk.android.model.entity.OneginiMobileAuthenticationRequest;
 
 import static com.onegini.mobile.exampleapp.view.activity.MobileAuthenticationFingerprintActivity.MSG_EXTRA_ASK_TO_ACCEPT_OR_DENY;
 import static com.onegini.mobile.exampleapp.view.activity.MobileAuthenticationFingerprintActivity.MSG_EXTRA_CLOSE;
 import static com.onegini.mobile.exampleapp.view.activity.MobileAuthenticationFingerprintActivity.MSG_EXTRA_RECEIVED_FINGERPRINT;
 import static com.onegini.mobile.exampleapp.view.activity.MobileAuthenticationFingerprintActivity.MSG_EXTRA_SHOW_SCANNING;
+
+import android.content.Context;
+import android.content.Intent;
+import com.onegini.mobile.exampleapp.view.activity.FingerprintActivity;
+import com.onegini.mobile.exampleapp.view.activity.MobileAuthenticationFingerprintActivity;
+import com.onegini.mobile.sdk.android.handlers.request.OneginiMobileAuthenticationFingerprintRequestHandler;
+import com.onegini.mobile.sdk.android.handlers.request.callback.OneginiFingerprintCallback;
+import com.onegini.mobile.sdk.android.model.entity.OneginiMobileAuthenticationRequest;
 
 public class MobileAuthenticationFingerprintRequestHandler implements OneginiMobileAuthenticationFingerprintRequestHandler {
 
