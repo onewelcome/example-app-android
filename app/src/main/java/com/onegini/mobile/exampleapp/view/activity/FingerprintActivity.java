03aa2280c4462d132b9ae8d85246a05862c93f49
Stein Welberg <stein@onegini.com>
2017-06-15 08:23:24 +0200

    SDKAND-240 Do finish the fingerprint activity when auth is cancelled

--- app/src/main/java/com/onegini/mobile/exampleapp/view/activity/FingerprintActivity.java
+++ app/src/main/java/com/onegini/mobile/exampleapp/view/activity/FingerprintActivity.java
@@ -102,6 +102,7 @@ public class FingerprintActivity extends AuthenticationActivity {
   protected void cancelRequest() {
     if(FingerprintAuthenticationRequestHandler.CALLBACK != null) {
       FingerprintAuthenticationRequestHandler.CALLBACK.denyAuthenticationRequest();
+      finish();
     }
   }
 }
