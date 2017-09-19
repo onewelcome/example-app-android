631852e1c5a7d10dc86637818039a778b86aeeb4
Marcin Paszylk <marcin@onegini.com>
2017-09-19 11:57:26 +0200

    SDKAND-326 add mobile auth with push status message

--- app/src/main/java/com/onegini/mobile/exampleapp/view/activity/SettingsActivity.java
+++ app/src/main/java/com/onegini/mobile/exampleapp/view/activity/SettingsActivity.java
@@ -160,6 +160,7 @@ public class SettingsActivity extends AppCompatActivity {
       @Override
       public void onSuccess() {
         mobileAuthPushButton.setText(R.string.settings_mobile_push_enrollment_on);
+        message.setText(R.string.enable_mobile_authentication_with_push_finished_successfully);
       }
 
       @Override
