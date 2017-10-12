7285b30a93f3aa0ce9206f2374148da248c4a65f
Robert Szuba <robert@onegini.com>
2017-10-12 10:54:33 +0200

    SDKAND-487 Show each push as a separate notification

--- app/src/main/java/com/onegini/mobile/exampleapp/network/fcm/NotificationHelper.java
+++ app/src/main/java/com/onegini/mobile/exampleapp/network/fcm/NotificationHelper.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2016-2017 Onegini B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.onegini.mobile.exampleapp.network.fcm;
 
 import android.app.Notification;
@@ -11,6 +27,7 @@ import android.os.Build;
 import android.support.annotation.RequiresApi;
 import android.support.v4.app.NotificationCompat;
 import com.onegini.mobile.exampleapp.R;
+import com.onegini.mobile.exampleapp.model.NotificationId;
 
 public class NotificationHelper {
 
@@ -34,7 +51,7 @@ public class NotificationHelper {
         .setPriority(NotificationCompat.PRIORITY_MAX)
         .setAutoCancel(true);
 
-    getManager().notify(1, builder.build());
+    getManager().notify(NotificationId.getId(), builder.build());
   }
 
   @RequiresApi(api = Build.VERSION_CODES.O)
