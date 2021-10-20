64021302e0d699b72efa167a1c666afd2e598057
Marcin Paszylk <marcin@onegini.com>
2021-10-20 14:14:08 +0200

    SDKAND-1353 revert security controller change

--- app/src/main/java/com/onegini/mobile/exampleapp/SecurityController.java
+++ app/src/main/java/com/onegini/mobile/exampleapp/SecurityController.java
@@ -2,7 +2,7 @@ package com.onegini.mobile.exampleapp;
 
 @SuppressWarnings({ "unused", "WeakerAccess" })
 public final class SecurityController {
-  public static final boolean rootDetection = false;
   public static final boolean debugDetection = false;
+  public static final boolean rootDetection = false;
   public static final boolean debugLogs = true;
 }
